0d0a4a0a0374e76fb83101eca0ef08483b999fd1
adevjoe <adevjoe@gmail.com>
2019-12-16 16:24:18 +0800

    finish letter combinations

--- algorithms/letter_combinations/letter_combinations.go
+++ algorithms/letter_combinations/letter_combinations.go
@@ -1,5 +1,38 @@
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
 package letter_combinations
 
+var letterMap map[string]string = map[string]string{
+	"2": "abc",
+	"3": "def",
+	"4": "ghi",
+	"5": "jkl",
+	"6": "mno",
+	"7": "pqrs",
+	"8": "tuv",
+	"9": "wxyz",
+}
+
 func letterCombinations(digits string) []string {
-	return nil
+	var result []string
+	for _, s := range digits {
+		result = combine(result, letterMap[string(s)])
+	}
+	return result
+}
+
+func combine(a []string, b string) []string {
+	var result []string
+	if len(a) == 0 {
+		for _, v := range b {
+			result = append(result, string(v))
+		}
+	} else {
+		for _, av := range a {
+			for _, bv := range b {
+				result = append(result, av+string(bv))
+			}
+		}
+	}
+	return result
 }
--- algorithms/letter_combinations/letter_combinations_test.go
+++ algorithms/letter_combinations/letter_combinations_test.go
@@ -3,5 +3,8 @@ package letter_combinations
 import "testing"
 
 func TestLetterCombinations(t *testing.T) {
-
+	t.Log(letterCombinations("1"))
+	t.Log(letterCombinations("2"))
+	t.Log(letterCombinations("23"))
+	t.Log(letterCombinations("789"))
 }
