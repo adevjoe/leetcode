671075ea91a4bff29590e7bf558c7af2d2a967f0
adevjoe <adevjoe@gmail.com>
2020-04-03 17:26:08 +0800

    add build tree

---
+++ algorithms/tree/construct_binary_tree_from_preorder_and_inorder_traversal.go
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+package tree
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	return buildTreeWithPoint(&preorder, inorder)
+}
+
+func buildTreeWithPoint(preorder *[]int, inorder []int) *TreeNode {
+	if len(inorder) == 0 {
+		return nil
+	}
+	p := *preorder
+	index := index(inorder, p[0])
+	*preorder = p[1:]
+	root := &TreeNode{Val: inorder[index]}
+	root.Left = buildTreeWithPoint(preorder, inorder[:index])
+	root.Right = buildTreeWithPoint(preorder, inorder[index+1:])
+	return root
+}
+
+func index(list []int, value int) int {
+	for i, v := range list {
+		if v == value {
+			return i
+		}
+	}
+	return -1
+}
