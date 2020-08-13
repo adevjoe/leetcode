ccc4c6b992dbf841480418b226e4256def75a917
adevjoe <adevjoe@gmail.com>
2020-08-13 17:29:50 +0800

    add sumNumbers

---
+++ algorithms/tree/sum_root_to_leaf_numbers.go
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/sum-root-to-leaf-numbers/
+
+package tree
+
+func sumNumbers(root *TreeNode) int {
+	return sumNumbersWithCur(root, 0, 0)
+}
+
+func sumNumbersWithCur(root *TreeNode, cur int, sum int) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		sum += cur*10 + root.Val
+	}
+	if root.Left != nil {
+		sum = sumNumbersWithCur(root.Left, cur*10+root.Val, sum)
+	}
+	if root.Right != nil {
+		sum = sumNumbersWithCur(root.Right, cur*10+root.Val, sum)
+	}
+	return sum
+}
---
+++ algorithms/tree/sum_root_to_leaf_numbers_test.go
@@ -0,0 +1,24 @@
+package tree
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSumNumbers(t *testing.T) {
+	cases := []struct {
+		arg1 *TreeNode
+		want int
+	}{
+		{arg1: &TreeNode{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3}}, want: 25},
+		{arg1: &TreeNode{Val: 4, Left: &TreeNode{Val: 9, Left: &TreeNode{Val: 5}, Right: &TreeNode{Val: 1}}, Right: &TreeNode{Val: 0}}, want: 1026},
+	}
+
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("TestSumNumbers-%d", i), func(t *testing.T) {
+			if get := sumNumbers(c.arg1); get != c.want {
+				t.Errorf("arg1: %v, want: %v, get: %v", preorderTraversal(c.arg1), c.want, get)
+			}
+		})
+	}
+}
