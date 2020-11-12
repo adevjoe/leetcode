bda22a916dc8698618785529ae40fd0cc05c4d94
adevjoe <adevjoe@gmail.com>
2020-11-12 18:01:09 +0800

    add lowest-common-ancestor-of-a-binary-search-tree

---
+++ algorithms/tree/lowest_common_ancestor_of_a_binary_search_tree_test.go
@@ -0,0 +1,55 @@
+package tree
+
+import "testing"
+
+func TestLowestCommonAncestor(t *testing.T) {
+	cases := []struct {
+		desc string
+		root *TreeNode
+		p    *TreeNode
+		q    *TreeNode
+		want *TreeNode
+	}{
+		{
+			desc: "#1",
+			root: &TreeNode{Val: 6, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 0}, Right: &TreeNode{Val: 4, Left: &TreeNode{Val: 3}, Right: &TreeNode{Val: 5}}}, Right: &TreeNode{Val: 8, Left: &TreeNode{Val: 7}, Right: &TreeNode{Val: 9}}},
+			p:    &TreeNode{Val: 2},
+			q:    &TreeNode{Val: 8},
+			want: &TreeNode{Val: 6, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 0}, Right: &TreeNode{Val: 4, Left: &TreeNode{Val: 3}, Right: &TreeNode{Val: 5}}}, Right: &TreeNode{Val: 8, Left: &TreeNode{Val: 7}, Right: &TreeNode{Val: 9}}},
+		},
+		{
+			desc: "#2",
+			root: &TreeNode{Val: 6, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 0}, Right: &TreeNode{Val: 4, Left: &TreeNode{Val: 3}, Right: &TreeNode{Val: 5}}}, Right: &TreeNode{Val: 8, Left: &TreeNode{Val: 7}, Right: &TreeNode{Val: 9}}},
+			p:    &TreeNode{Val: 2},
+			q:    &TreeNode{Val: 4},
+			want: &TreeNode{Val: 2, Left: &TreeNode{Val: 0}, Right: &TreeNode{Val: 4, Left: &TreeNode{Val: 3}, Right: &TreeNode{Val: 5}}},
+		},
+		{
+			desc: "#3",
+			root: &TreeNode{Val: 2, Left: &TreeNode{Val: 1}},
+			p:    &TreeNode{Val: 2, Left: &TreeNode{Val: 1}},
+			q:    &TreeNode{Val: 1},
+			want: &TreeNode{Val: 2, Left: &TreeNode{Val: 1}},
+		},
+		{
+			desc: "#4",
+			root: &TreeNode{Val: 3, Left: &TreeNode{Val: 1, Right: &TreeNode{Val: 2}}, Right: &TreeNode{Val: 4}},
+			p:    &TreeNode{Val: 2},
+			q:    &TreeNode{Val: 3},
+			want: &TreeNode{Val: 3, Left: &TreeNode{Val: 1, Right: &TreeNode{Val: 2}}, Right: &TreeNode{Val: 4}},
+		},
+	}
+
+	for _, s := range cases {
+		t.Run(s.desc, func(t *testing.T) {
+			if got := lowestCommonAncestor(s.root, s.p, s.q); !isSameTree(got, s.want) {
+				t.Errorf("want: %v, got: %v", inorderTraversal(s.want), inorderTraversal(got))
+			}
+		})
+		t.Run(s.desc, func(t *testing.T) {
+			if got := lowestCommonAncestor2(s.root, s.p, s.q); !isSameTree(got, s.want) {
+				t.Errorf("want: %v, got: %v", inorderTraversal(s.want), inorderTraversal(got))
+			}
+		})
+	}
+}
