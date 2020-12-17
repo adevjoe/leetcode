02d86d9f78de77f51d3429296667dd4657adcdcc
adevjoe <adevjoe@gmail.com>
2020-12-17 12:43:43 +0800

    add lru cache

--- algorithms/lru_cache.go
+++ algorithms/lru_cache.go
@@ -1,3 +1,111 @@
 // https://leetcode.com/problems/lru-cache/
 
 package algorithms
+
+type LRUCache struct {
+	values   map[int]*lruDoubleListNode
+	head     *lruDoubleListNode
+	tail     *lruDoubleListNode
+	capacity int
+}
+
+type lruDoubleListNode struct {
+	key   int
+	value int
+	pre   *lruDoubleListNode
+	next  *lruDoubleListNode
+}
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		values:   map[int]*lruDoubleListNode{},
+		capacity: capacity,
+	}
+}
+
+func (this *LRUCache) Get(key int) int {
+	v, ok := this.values[key]
+	if !ok {
+		return -1
+	}
+	this.moveAhead(v)
+	return v.value
+}
+
+func (this *LRUCache) moveAhead(node *lruDoubleListNode) {
+	if this.head == this.tail {
+		this.head = node
+		this.tail = node
+		return
+	}
+	if node == this.head {
+		return
+	}
+	if node == this.tail {
+		this.tail = node.pre
+	}
+	if node.pre != nil {
+		node.pre.next = node.next
+	}
+	if node.next != nil {
+		node.next.pre = node.pre
+	}
+	node.pre = nil
+	node.next = this.head
+	this.head.pre = node
+	this.head = node
+}
+
+func (this *LRUCache) putHead(node *lruDoubleListNode) {
+	if this.head == nil {
+		this.head = node
+		if this.tail == nil {
+			this.tail = node
+		}
+		return
+	}
+	this.head.pre = node
+	node.next = this.head
+	this.head = node
+}
+
+func (this *LRUCache) deleteTail() {
+	if this.head == this.tail {
+		this.head = nil
+		this.tail = nil
+		return
+	}
+	this.tail = this.tail.pre
+	this.tail.next = nil
+}
+
+func (this *LRUCache) Put(key int, value int) {
+	// update value
+	if v, ok := this.values[key]; ok {
+		this.values[key].value = value
+		this.moveAhead(v)
+		return
+	}
+
+	if len(this.values) == this.capacity {
+		// evict
+		delete(this.values, this.tail.key)
+		this.deleteTail()
+	}
+
+	// put
+	node := &lruDoubleListNode{
+		key:   key,
+		value: value,
+	}
+	this.putHead(node)
+	this.values[key] = node
+	return
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
--- algorithms/lru_cache_test.go
+++ algorithms/lru_cache_test.go
@@ -1,1 +1,52 @@
 package algorithms
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLRUCache(t *testing.T) {
+	l := Constructor(2)
+	l.Put(1, 1)
+	printDoubleList(l.head, l.tail)
+	l.Put(2, 2)
+	printDoubleList(l.head, l.tail)
+	if got := l.Get(1); got != 1 {
+		t.Errorf("set 1 error, got: %d", got)
+	}
+	l.Put(3, 3)
+	printDoubleList(l.head, l.tail)
+	if got := l.Get(2); got != -1 {
+		t.Errorf("evict 1 error, got: %d", got)
+	}
+	l.Put(4, 4)
+	if got := l.Get(1); got != -1 {
+		t.Errorf("evict 1 error, got: %d", got)
+	}
+	if got := l.Get(3); got != 3 {
+		t.Errorf("set 3 error, got: %d", got)
+	}
+	if got := l.Get(4); got != 4 {
+		t.Errorf("set 4 error, got: %d", got)
+	}
+}
+
+func printDoubleList(head, tail *lruDoubleListNode) {
+	s := ""
+	n := ""
+	for head != nil {
+		if s != "" {
+			s += "->"
+		}
+		s = fmt.Sprintf("%s%d", s, head.value)
+		head = head.next
+	}
+	for tail != nil {
+		if n != "" {
+			n += "->"
+		}
+		n = fmt.Sprintf("%s%d", n, tail.value)
+		tail = tail.pre
+	}
+	fmt.Println(s, "|", n)
+}
