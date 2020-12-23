63bce746e50e0b76eab9dc2a14a705059939beb8
adevjoe <adevjoe@gmail.com>
2020-12-23 17:32:15 +0800

    Add expression add operators

--- algorithms/expression_add_operators.go
+++ algorithms/expression_add_operators.go
@@ -10,12 +10,12 @@ func addOperators(num string, target int) []string {
 	if num == "" {
 		return nil
 	}
-	result = make([]string, 0)
-	addOperatorsSub(num, target, 0, 0, "")
+	result = nil
+	addOperatorsSub(num, target, 0, 0, 0, "")
 	return result
 }
 
-func addOperatorsSub(num string, target int, pre, pos int, path string) {
+func addOperatorsSub(num string, target int, pre, preValue, pos int, path string) {
 	if pos == len(num) {
 		if pre == target {
 			result = append(result, path)
@@ -23,13 +23,16 @@ func addOperatorsSub(num string, target int, pre, pos int, path string) {
 		return
 	}
 	for i := pos; i < len(num); i++ {
+		if len(num[pos:i+1]) > 1 && num[pos] == '0' {
+			continue
+		}
 		n, _ := strconv.Atoi(num[pos : i+1])
 		if pos == 0 {
-			addOperatorsSub(num, target, n, i+1, num[pos:i+1])
+			addOperatorsSub(num, target, n, n, i+1, num[pos:i+1])
 		} else {
-			addOperatorsSub(num, target, pre+n, i+1, path+"+"+num[pos:i+1])
-			addOperatorsSub(num, target, pre-n, i+1, path+"-"+num[pos:i+1])
-			addOperatorsSub(num, target, pre*n, i+1, path+"*"+num[pos:i+1])
+			addOperatorsSub(num, target, pre+n, n, i+1, path+"+"+num[pos:i+1])
+			addOperatorsSub(num, target, pre-n, -n, i+1, path+"-"+num[pos:i+1])
+			addOperatorsSub(num, target, pre-preValue+preValue*n, preValue*n, i+1, path+"*"+num[pos:i+1])
 		}
 	}
 }
